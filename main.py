192f5af25ef4651b218c35d34ef9b3207409426a
Rajesh Madugula <rajeshmaduugla01@gmail.com>
2025-02-05 16:00:37 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.chicken_disease_classification_project import logger
+from src.chicken_disease_classification_project.logger import logger
+from src.chicken_disease_classification_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom logg")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
